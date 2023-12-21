9ace20fe282d273ad19efe609d4ca25d80adcebd
Denis Fournier de Carvalho <denisfournier1994@gmail.com>
2023-12-21 02:32:08 -0300

    interface estatica.

---
+++ sistema/lojista/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel do Lojista</title><link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="menu">
+            <a href="#" class="item item-delivery">
+                <div>DELIVERY</div>
+            </a>
+            <a href="#" class="item item-retirada">
+                <div>RETIRADA</div>
+            </a>
+            <a href="#" class="item item-mesa">
+                <div>MESA</div>
+            </a>
+            <a href="#" class="item item-sair">
+                <div>SAIR</div>
+            </a>
+        </div>
+        <div class="content">
+            <div class="banner"></div>
+            <div class="search">
+                <input type="text" class="searchbar" placeholder="Procure o seu pedido por qualquer campo que desejar.">
+                <!-- <input type="text" class="garcom" placeholder="Digite o nome do garçom">
+                <input type="text" class="comanda" placeholder="Digite o numero da mesa ou comanda"> -->
+            </div>
+            <div class="control">
+                <div class="cotrol-buttons">
+                    <a href="#" class="bt-link todos">Todos</a>
+                    <a href="#" class="bt-link aberto">Em aberto</a>
+                    <a href="#" class="bt-link produzindo">Produzindo</a>
+                    <a href="#" class="bt-link entrega">Saiu para entrega</a>
+                    <a href="#" class="bt-link finalizados">Finalizados</a>
+                    <a href="#" class="bt-link cancelados">Cancelados</a>
+                </div>
+                <div class="total">Total de pedidos: 100</div>
+            </div>
+        
+            <iframe src="teste.html" frameborder=0>
+                <p>Your browser does not support iframes.</p>
+            </iframe>
+        </div>
+    </div>
+</body>
+</html>
